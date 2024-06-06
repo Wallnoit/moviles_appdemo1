078dd5de251b6dbb315bdf20f0ab7b6d9c7a6964
WilliamPerez2002 <wulli.mu28@gmail.com>
2024-06-06 14:20:42 -0500

    primer commit

---
+++ app/src/main/java/com/example/app_android_demo1/WelcomeActivity.kt
@@ -0,0 +1,34 @@
+package com.example.app_android_demo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.app_android_demo1.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        imprimirMensaje()
+    }
+
+    fun imprimirMensaje() {
+
+        with(binding) {
+            btnConfirmar.setOnClickListener {
+                val nombre = editNombres.text.toString()
+                val apellido = editTextText2.text.toString()
+                val mensaje = "Bienvenido $nombre $apellido"
+                resultado.text = mensaje
+            }
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/app_android_demo1/components.kt
@@ -0,0 +1,12 @@
+package com.example.app_android_demo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class components : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_components)
+
+    }
+}
